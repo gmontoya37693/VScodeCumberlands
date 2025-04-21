a46dc89b7a9bfff0c9e3a6cacb16586c8e1db35a
German Montoya <damoser@hotmail.com>
2025-04-21 13:45:34 -0400

    aceptado

--- input_raw.py
+++ input_raw.py
@@ -1,18 +1,24 @@
 names = []
 assignments = []
 grades = []
-
-num_students = 3
+student = 1
+num_students = 4
 
 while student <= num_students:
     name = input("Enter student name {}: ".format(student))
     names.append(name)
-    assignment = input("Enter assignment name {}: ".format(student))
+    assignment = int(input("Enter number of assignments left for {}: ".format(name)))
     assignments.append(assignment)
-    grade = input("Enter grade for {}: ".format(name))
+    grade = int(input("Enter grade for {}: ".format(name)))
     grades.append(grade)
     student += 1
 
-    print(names)
-    print(assignments)
-    print(grades)
+for n in range(num_students):
+    potential_grade = grades[n] + (assignments[n] * 2)  # Example calculation
+    print(
+        "Hi {},\n\nThis is a reminder that you have {} assignments left to "
+        "submit before you can graduate. Your current grade is {} and it can increase "
+        "to {} if you submit all assignments before the due date.\n\n".format(
+            names[n], assignments[n], grades[n], potential_grade
+        )
+    )
