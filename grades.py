25710cf3f3a2163f4dcefe3df5e72fffa2495ca5
German Montoya <damoser@hotmail.com>
2025-04-21 17:56:47 -0400

    mine version

--- grades.py
+++ grades.py
@@ -1,13 +1,13 @@
-names = input("Enter names separated with commas: ").title.split("'")
-assignments = int(input("Enter assignments separated with commas: ").split(","))
-grades = int(input("Enter grades separated with commas: ").split(","))
+names = input("Enter names separated with commas: ").title().split(",")
+assignments = input("Enter assignments separated with commas: ").split(",")
+grades = input("Enter grades separated with commas: ").split(",")
 
 message = "Hi {},\n\nThis is a reminder that you have {} assignments left to \
-submit before you can graduate. You're current grade is {} and can increase \
+submit before you can graduate. Your current grade is {} and can increase \
 to {} if you submit all assignments before the due date.\n\n"
 
 for name, assignment, grade in zip(names, assignments, grades):
-    potential_grade = grade + assignments * 2
+    potential_grade = int(grade) + int(assignment) * 2
     print(message.format(name, assignment, grade, potential_grade))
 # This code takes a list of names, assignments, and grades from the user and
 # generates a personalized message for each student. The message includes
