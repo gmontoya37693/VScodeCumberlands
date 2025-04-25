4e516103c61eeab6e5762668c1f16cdb80b03ca8
German Montoya <damoser@hotmail.com>
2025-04-25 15:18:15 -0400

    open + read + write + close

--- open_read.py
+++ open_read.py
@@ -2,3 +2,9 @@ f = open('some_file.txt')
 file_data = f.read()
 print(file_data)
 f.close()
+# The above code opens a file named 'some_file.txt', reads its content, and prints it to the console.
+# The file is then closed.
+
+f.open('some_file.txt', 'w')
+f.write('Hello, Germanchirris!')
+f.close()
