ce7b70d1c37120ef7140562344b377dc86fc6a85
German Montoya <damoser@hotmail.com>
2025-04-25 12:08:05 -0400

    first version of ventiuna.py

---
+++ ventiuna.py
@@ -0,0 +1,6 @@
+deck = input('Enter cards from the deck separated by commas').split(',')
+
+print('The cards in the deck are: {}'.format(deck))
+print('Deck summ = ', sum(deck))
+print('Card missing to blackjack = ', 21 - sum(deck))
+
