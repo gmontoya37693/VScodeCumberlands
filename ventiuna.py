82adfd1ae9b3bd0e122c5b846716ef887edeeb7e
German Montoya <damoser@hotmail.com>
2025-04-25 12:18:24 -0400

    1

--- ventiuna.py
+++ ventiuna.py
@@ -1,3 +1,6 @@
-deck = input('Enter cards from deck separated by commas').split(',')
-
+deck = input('Enter cards from deck separated by commas: ').split(',')
+deck = list(map(int, deck))
+# Convert the deck elements to integers
 print('Deck:', deck)
+print('Deck length:', len(deck))
+print('Deck sum:', sum(deck))
