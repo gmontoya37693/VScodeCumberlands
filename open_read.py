4f0872a6370375e1a68a45b30d24ced711db4f31
German Montoya <damoser@hotmail.com>
2025-04-25 15:42:36 -0400

    append

--- open_read.py
+++ open_read.py
@@ -5,7 +5,7 @@ f.close()
 # The above code opens a file named 'some_file.txt', reads its content, and prints it to the console.
 # The file is then closed.
 
-g = open('some_file.txt', 'w')
-file_data = g.write('______')
+g = open('some_file.txt', 'a')
+file_data = g.write('\nHello, this is a new line.\n')
 print(file_data)
 g.close()
