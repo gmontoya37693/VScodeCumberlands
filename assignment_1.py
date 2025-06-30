f33b4462d7f9469a07dc460340c6c97859baaa18
German Montoya <damoser@hotmail.com>
2025-06-30 13:43:55 -0400

    assignment_1.py with comments

--- assignment_1.py
+++ assignment_1.py
@@ -7,7 +7,8 @@ German Montoya
 name = input("What is your name: ")
 print(name)
 while True:
-    test = input("Do you know Python? (yes/no): ").lower()
+    test = input("Do you know Python? (yes/no): ").lower() # Convert input to lowercase for consistency
+    # Check if the input is either "yes" or "no"
     if test == "yes" or test == "no":
         break
     print("Please answer with 'yes' or 'no'.")
