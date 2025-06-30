a557ada970f874157aa056a0046e94970c1dcede
German Montoya <damoser@hotmail.com>
2025-06-30 12:57:04 -0400

    create new file working_directory_check.py

---
+++ working_directory_check.py
@@ -0,0 +1,12 @@
+"""
+File created to validate the working diretory.
+Executing this script file will print the location of
+this script file, if VSC was setup as directed.
+
+06/30/2023
+German Montoya
+"""
+
+import os # module for OS interaction 
+
+print("\nThe current directory is", os.getcwd(), "\n")
