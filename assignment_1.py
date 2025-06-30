8a21ad66f9e9488ecb823c62434364367a06f31c
German Montoya <damoser@hotmail.com>
2025-06-30 13:30:06 -0400

    assignment_1.py

---
+++ assignment_1.py
@@ -0,0 +1,14 @@
+"""
+Assignment 1: this is a basic input and output file
+June 30, 2025
+German Montoya
+"""
+
+name = input("What is your name: ")
+print(name)
+while True:
+    test = input("Do you know Python? (yes/no): ").lower()
+    if test == "yes" or test == "no":
+        break
+    print("Please answer with 'yes' or 'no'.")
+print("Thanks for answering my questions, {}.".format(name))
