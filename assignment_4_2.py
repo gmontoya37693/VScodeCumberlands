c68bef9c56f18257054665a8a9aff341da813006
German Montoya <damoser@hotmail.com>
2025-07-30 13:25:52 -0400

    final version of assignment_4_2.py

--- assignment_4_2.py
+++ assignment_4_2.py
@@ -22,4 +22,22 @@ def allMatchesIndices(srch_str, sub_str):
             break
         indices.append(pos) # Append the found start index to the list
         start = pos + 1  # Move start by 1 to allow overlapping matches
-    return tuple(indices)   
+    return tuple(indices)
+
+# Example usage
+# The following code allows the user to input a string and a substring
+# Cycles through the process until the user decides to stop.
+while True:
+    test = input("Do you want to find all substring indices? (yes/no): ").lower()
+    if test == "no":
+        print("Thank you for using the program.")
+        break
+    elif test == "yes":
+        srch_str = input("Enter the string to search in: ")
+        sub_str = input("Enter the substring to find: ")
+        indices = allMatchesIndices(srch_str, sub_str)
+        print(f"allMatchesIndices({srch_str!r}, {sub_str!r})")
+        print(indices)
+        print()  # Add a blank line after each run
+    else:
+        print("Invalid input. Please enter 'yes' or 'no'.")
