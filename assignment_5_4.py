0309882f7dcfd6e8b930f7981bc0845056c95713
German Montoya <damoser@hotmail.com>
2025-08-07 16:25:08 -0400

    draft

---
+++ assignment_5_4.py
@@ -0,0 +1,11 @@
+"""
+Assignment 5 Problem 4: Track how the account balance changes 
+due to an initial balance, a fixed annual withdrawal, and a rate
+of return.
+August 6, 2025
+Germán Montoya
+"""
+
+def maximumExpensed(salary, p_rate, workRate, f_rate, retiredRate, epsilon):
+    """
+    """
