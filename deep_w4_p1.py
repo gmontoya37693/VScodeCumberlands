9e65c8d77c5c3d1c6a4527866101f004218527a4
German Montoya <damoser@hotmail.com>
2026-01-29 12:38:02 -0500

    header

--- deep_w4_p1.py
+++ deep_w4_p1.py
@@ -1,6 +1,13 @@
 """
 Programming Exercise 1: Optimizing an approximation line
 2026 Spring - Deep Learning (MSDS-534-M40) - Full Term
+Professor: Dr. Intisar Rizwan I Haque
 January 28, 2026
 Germán Montoya
+
+Objective:
+Implement a gradient-based optimization algorithm to minimize the function:
+f(x) = 0.5 * ||A * x - b||^2
+where A, x, and b are vectors. This is a least squares optimization problem
+commonly used in machine learning for finding optimal parameters.
 """
